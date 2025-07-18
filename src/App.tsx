92b44cf91076a95da4a49dbb97d8d2fb1774a0f6
kira-coding <kirubelabeben@gmail.com>
2025-07-18 23:09:40 +0300

    disable asset loading protocol

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 // import { invoke } from "@tauri-apps/api/core";
 
-import { startListening, onClipboardChange, stopListening, writeFiles, writeImage, writeText } from "tauri-plugin-clipboard-x-api";
+import { startListening, onClipboardChange, stopListening, writeFiles, writeImage, writeText, getDefaultSaveImagePath } from "tauri-plugin-clipboard-x-api";
 import Database from '@tauri-apps/plugin-sql';
 import Search from "../components/Search";
 import Filter from "../components/Filter";
@@ -100,7 +100,9 @@ function App() {
       setAutoStart(start)
     }
     listenToClipboard();
-    manageAutostart()
+    manageAutostart();
+    console.log(getDefaultSaveImagePath())
+
   }, [])
 
   useEffect(() => {
@@ -130,7 +132,7 @@ function App() {
       setClipboard(data)
     }
     const Copy = async () => {
-      if (selectedData.type == "text"|| selectedData.type=="link") {
+      if (selectedData.type == "text" || selectedData.type == "link") {
         await writeText(selectedData.value)
       }
       else if (selectedData.type == "image") {
@@ -228,10 +230,10 @@ function App() {
           <input onChange={() => {
             setAutoStart(!autoStart)
             setMode("Startup")
-          }} type="checkbox"  checked={autoStart}  className="peer h-5 w-5 cursor-pointer transition-all appearance-none rounded shadow hover:shadow-md border border-slate-300 checked:bg-slate-800 checked:border-slate-800" id="check" />
+          }} type="checkbox" checked={autoStart} className="peer h-5 w-5 cursor-pointer transition-all appearance-none rounded shadow hover:shadow-md border border-slate-300 checked:bg-slate-800 checked:border-slate-800" id="check" />
           <span className="absolute text-white opacity-0 peer-checked:opacity-100 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 pointer-events-none">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor" stroke="currentColor" stroke-width="1">
-              <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor" stroke="currentColor" strokeWidth="1">
+              <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path>
             </svg>
           </span>
         </label>
@@ -341,3 +343,4 @@ function App() {
   );
 }
 export default App;
+
